3db0ec03d673f99b9495e8951fc7af107e7e3a2b
dianaandrade2022 <a3521110157@alumno.uttehuacan.edu.mx>
2024-07-07 22:10:46 -0600

    new changes

--- src/app/page.js
+++ src/app/page.js
@@ -1,39 +1,86 @@
 "use client";
-import { Stack, TextField, Typography, Button } from "@mui/material";
+import { Stack, TextField, Typography, Button, Input } from "@mui/material";
 import SendIcon from "@mui/icons-material/Send";
 import { useEffect, useState } from "react";
 import { io } from "socket.io-client";
 import { v4 as uuidv4 } from "uuid";
-const socket = io("http://localhost:9001");
-import {es} from 'date-fns/locale'
+import { es } from "date-fns/locale";
 import { formatRelative, subDays } from "date-fns";
+
+const socket = io("http://localhost:9001");
 
 export default function Home() {
   const [message, setMessage] = useState("");
 
   // no se actualizará el estado por lo cual se elimina setId
   const [id] = useState(uuidv4());
-  const [listmessage, setListMessage] = useState([]);
-  const [fecha ] = useState([]);
+  const [listMessage, setListMessage] = useState([]);
+  const [file, setFile] = useState(null);
+  const [error, setError] = useState(null);
+  const [image, setImage] = useState(null);
+
+  async function handleClick(e) {
+    e.preventDefault();
+
+    if (file) {
+      if (!file.type.startsWith("image/")) {
+        setError("Solo se permiten imágenes.");
+        setFile(null);
+        return;
+      }
+
+      const formData = new FormData();
+      formData.append("image", file);
+
+      const res = await fetch("/api/upload", {
+        method: "POST",
+        body: formData,
+      });
+
+      if (!res.ok) {
+        throw new Error("Error al subir la imagen");
+      }
+
+      const data = await res.json();
+      
   
-  function handleclick() {
-    const fecha =  formatRelative(subDays(new Date(), 0), new Date(), { locale: es })
-    console.log(fecha);
-    const text = message;
+      console.log(data.url);
+      console.log(data);
+
+    }
+
+    const fecha = formatRelative(subDays(new Date(), 0), new Date(), {
+      locale: es,
+    });
+
+
     socket.emit("newMessage", {
       id,
-      message: text,
-      fechamessage:fecha
+      message: message || "",
+      fechamessage: fecha,
+      archivo: image ? image : null,
     });
+
     setMessage("");
-  } 
+    setFile(null);
+    setError(null);
+  }
+
+  function getFile(e) {
+    const data = e.target.files[0];
+    setFile(data);
+    setImage(image)
+    setError(null);
+  }
 
   useEffect(() => {
     socket.on("messages", (data) => {
-      setListMessage((prevListMessages) =>
-        prevListMessages.concat(data)
-      );
+      setListMessage((prevListMessages) => prevListMessages.concat(data));
+      setImage(data.url);  
+      console.log("desde useeffect",data.url);
+      console.log("desde useeffect",data);
     });
+    
   }, []);
 
   return (
@@ -44,57 +91,69 @@ export default function Home() {
     >
       <Stack
         sx={{
-          backgroundColor: "#EB1700",
+          backgroundColor: "#FFF",
           height: "60px",
           justifyContent: "center",
           alignItems: "center",
           flexShrink: 0,
+          borderRadius: 5,
         }}
         direction="row"
       >
-        <Typography 
-        sx={{
-          color:"#fff",
-          fontWeight:"bold",
-          fontSize:"20px"
-        }}
-        >Chat Online</Typography>
-        
+        <Typography
+          sx={{
+            color: "#54555B",
+            fontSize: "25px",
+            fontWeight: "bolder",
+          }}
+        >
+          Chat Online
+        </Typography>
       </Stack>
 
       <Stack
         sx={{
-          backgroundColor: "#000",
+          backgroundColor: "#626FE3",
           flexGrow: 1,
           paddingBlock: "10px",
         }}
         spacing={2}
       >
-        {listmessage.map((item) => (
+        {listMessage.map((item, index) => (
           <Stack
+            key={index}
             sx={{
               maxWidth: "60%",
               minHeight: "30px",
-              backgroundColor: "white",
+              backgroundColor: item.message ? "white" : "transparent",
               borderRadius: "15px",
               alignSelf: item.id === id ? "end" : "start",
               p: 1,
             }}
           >
-            <Typography>{item.message}</Typography>
+            {item.message && <Typography>{item.message}</Typography>}
+            {item.archivo && (
+              <img
+                src={item.archivo}
+                alt="Uploaded"
+                style={{ maxWidth: "100%", maxHeight: "300px", marginTop: "10px" }}
+              />
+            )}
             <Typography
-            sx={{
-              alignSelf:"end",
-              color:"gray",
-              fontSize:9
-            }}
-            >{item.fechamessage}</Typography>
+              sx={{
+                alignSelf: "end",
+                color: item.message ? "gray":"black",
+                fontSize: 9,
+              }}
+            >
+              {item.fechamessage}
+            </Typography>
           </Stack>
         ))}
       </Stack>
 
       <Stack
-        sx={{  
+        sx={{
           height: "60px",
           alignItems: "center",
         }}
@@ -106,16 +165,27 @@ export default function Home() {
           fullWidth
           value={message}
           onChange={(e) => setMessage(e.target.value)}
-        ></TextField>
-
+        />
+        <Input type="file" onChange={getFile} />
         <Button
+          onClick={handleClick}
           variant="contained"
+          disabled={message.trim() === "" && !file}
           endIcon={<SendIcon />}
-          onClick={handleclick}
-        >
-          send
-        </Button>
+        />
       </Stack>
+
+      {error && (
+        <Typography
+          sx={{
+            color: "red",
+            textAlign: "center",
+            mt: 2,
+          }}
+        >
+          {error}
+        </Typography>
+      )}
     </Stack>
   );
 }
